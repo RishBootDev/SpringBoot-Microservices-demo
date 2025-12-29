348f47262333e4d1c684c8e47d9f56f6e5fbde77
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-29 13:42:21 +0530

    Initial commit

---
+++ InventoryService/src/test/java/com/rishbootdev/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishbootdev.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
