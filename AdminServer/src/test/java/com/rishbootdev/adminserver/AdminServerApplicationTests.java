65ba2bb13d2b502e58732d7b588cc507702862df
Rishabh <rishabhdubey1472004@gmail.com>
2026-01-07 16:38:32 +0530

    added the admin server

---
+++ AdminServer/src/test/java/com/rishbootdev/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rishbootdev.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
